9b222adfccbd32a8c49c3509031a16d86e5b09e3
Dima Dmitriev <komatic.dda@gmail.com>
2020-11-26 22:03:58 +0200

    cart JS project

---
+++ js/script.js
@@ -0,0 +1,79 @@
+window.addEventListener('DOMContentLoaded', function () {
+    let products = document.querySelectorAll('.product'),
+    buttons = document.querySelectorAll('button'),
+    openBtn = document.querySelector('.open');
+
+    function createCart() {
+        let cart = document.createElement('div'),
+            field = document.createElement('div'),
+            heading = document.createElement('h2'),
+            closeBtn = document.createElement('button');
+
+        cart.classList.add('cart');
+        field.classList.add('cart-field');
+        closeBtn.classList.add('close');
+
+        heading.textContent = "In the our cart: ";
+        closeBtn.textContent = "Close";
+
+        // add element - create - on the page
+        document.body.appendChild(cart);
+        cart.appendChild(heading);
+        cart.appendChild(field);
+        cart.appendChild(closeBtn);
+    }
+    
+    createCart();
+
+    let field = document.querySelector('.cart-field'),
+        cart = document.querySelector('.cart'),
+        closeBtn = document.querySelector('.close');
+        
+    function openCart() {
+        cart.style.display = 'block';
+    }
+
+    function closeCart() {
+        cart.style.display = 'none';
+    }
+
+    openBtn.addEventListener('click', openCart);
+    closeBtn.addEventListener('click', closeCart);
+
+
+    // first variant
+    /* for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function () {
+            let item = products[i].cloneNode(true),
+                btn = item.querySelector('button');
+            
+            btn.remove();
+            field.appendChild(item);
+            products[i].remove();
+        });
+    } */
+
+    // second variant
+    /* buttons.forEach(function (item, i) {
+        item.addEventListener('click', function () {
+            let item = products[i].cloneNode(true),
+                btn = item.querySelector('button');
+            
+            btn.remove();
+            field.appendChild(item);
+            products[i].remove();
+        });
+    }); */
+
+    // third variant
+    buttons.forEach((item, i) => {
+        item.addEventListener('click', () => {
+            let item = products[i].cloneNode(true),
+                btn = item.querySelector('button');
+            
+            btn.remove();
+            field.appendChild(item);
+            products[i].remove();
+        });
+    });
+});
